60b96cb4ce44003268e84c4da0052efd9f19b32d
wangshuai <wangshuai@bcbook.cn>
2018-10-29 16:46:47 +0800

    猜数字游戏

---
+++ src/com/ws/demo1/GameLauncher.java
@@ -0,0 +1,8 @@
+package com.ws.demo1;
+
+public class GameLauncher {
+    public static void main(String [] args){
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+}
---
+++ src/com/ws/demo1/Test1.java
@@ -0,0 +1,7 @@
+package com.ws.demo1;
+
+public class Test1 {
+    public static void main(String [] args){
+        System.out.println((int)(Math.random()*10));
+    }
+}
